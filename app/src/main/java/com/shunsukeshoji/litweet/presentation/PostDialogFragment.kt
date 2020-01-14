e01b026508328172ac048bc20bb5eeefe84464f9
sshun04 <shojishunshun@gmail.com>
2020-01-15 00:21:47 +0900

    create custom dialog

--- app/src/main/java/com/shunsukeshoji/litweet/presentation/PostDialogFragment.kt
+++ app/src/main/java/com/shunsukeshoji/litweet/presentation/PostDialogFragment.kt
@@ -1,4 +1,38 @@
 package com.shunsukeshoji.litweet.presentation
 
-class PostDialogFragment {
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.shunsukeshoji.litweet.R
+import kotlinx.android.synthetic.main.fragment_dialog_post.view.*
+
+class PostDialogFragment(
+    private val searchIds: List<String>,
+    private val submit: (String) -> Unit
+) : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val parentView =
+            requireActivity().layoutInflater.inflate(R.layout.fragment_dialog_post, null)
+
+        val dialog = AlertDialog.Builder(requireContext())
+            .setView(parentView)
+            .create()
+
+        parentView.buttonSubmit.setOnClickListener {
+            val id = parentView.editText.text.toString()
+            if (searchIds.contains(id)) {
+                submit(id)
+            } else {
+                parentView.editTextField.error = "このアカウントは存在しません"
+            }
+        }
+
+        parentView.buttonCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        return dialog
+    }
 }
